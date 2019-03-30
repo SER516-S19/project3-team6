70cffe204ef160b027fd214e04ff1b63d2631239
Abhinab <amohant7@asu.edu>
2019-03-30 11:07:19 -0700

    Initial project boiler plate for both apps

---
+++ StudentApp/src/main/java/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+    /**
+     * The main method to run the application
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Main Here");
+    }
+}

a3f3a50d69592db50fd242ebf4711bd1a44cada6
Gangadhara Matti <gangadharmatti.16@gmail.com>
2019-04-11 15:58:42 -0700

    Added code to read a file and convert it to JSONObject and added models for quiz and questions

--- ProfessorApp/src/main/java/service/FileReadService.java
+++ ProfessorApp/src/main/java/service/FileReadService.java
@@ -1,19 +1,48 @@
 package service;
 
-import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import org.json.JSONObject;
 
-import org.json.simple.JSONObject;
+/**
+ * Class for reading json file in the form of JSONObject
+ *
+ * @author gmatti1
+ * date - April 11, 2019
+ * version = 0.1
+ *
+ * */
 
 public class FileReadService {
 
 	public FileReadService() {
 	}
 
-	private static JSONObject readFile(File file) {
-		
-		return null;
+	public static JSONObject readFile(String file) {
+		String jsonString =  readJsonFile(file);
+		JSONObject jsonObject = new JSONObject(jsonString.substring(jsonString.indexOf('{')));
+		return jsonObject;
 	}
 
-
-
+	/*
+	* Get the file content in the form of String
+	* */
+	private static String readJsonFile(String filename) {
+		String jsonString = "";
+		try {
+			@SuppressWarnings("resource")
+			BufferedReader br = new BufferedReader(new FileReader(filename));
+			StringBuilder sb = new StringBuilder();
+			String readLine = br.readLine();
+			while(readLine != null) {
+				sb.append(readLine);
+				readLine = br.readLine();
+			}
+			jsonString = sb.toString();
+		}
+		catch(Exception ex) {
+			ex.printStackTrace();
+		}
+		return jsonString;
+	}
 }
